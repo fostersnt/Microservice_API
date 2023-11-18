dd7dbf1bb68d4d3b62428c8401fa47dbf5807b02
= <=>
2023-11-18 15:59:37 +0000

    Added Book model

---
+++ Book_Service/Model/Book.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Service.Model
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
