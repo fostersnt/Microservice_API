b7e8498695a2afd0e37a88abbd3baa600d9c8e97
= <=>
2023-11-18 16:26:49 +0000

    Added common API response class

---
+++ Book_Service/DTO/CommonResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Book_Service.DTO
+{
+    public class CommonResponse
+    {
+        public object? Result { get; set; }
+        public bool isSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
