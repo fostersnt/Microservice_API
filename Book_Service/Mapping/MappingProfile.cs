a49c170984067b6de73c69f5b29fd3bb624f0f6b
= <=>
2023-11-18 16:40:18 +0000

    Added mapping to Book

---
+++ Book_Service/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Book_Service.DTO;
+using Book_Service.Model;
+
+namespace Book_Service.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Book, BookDTO>();
+
+        }
+    }
+}
