574ff9948d1a42d5c2e933d9a37d979055097898
= <=>
2023-11-18 16:19:40 +0000

    Added onModelCreating to the BookDbContext class

---
+++ Book_Service/Data/BookAppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Book_Service.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Service.Data
+{
+    public class BookAppDbContext : DbContext
+    {
+        public BookAppDbContext(DbContextOptions<BookAppDbContext> options) :base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Book>().HasKey(b => b.BookId);
+            modelBuilder.Entity<Book>().HasData(
+                new Book{ BookId = 1, Name = "The Spider" },
+                new Book{ BookId = 2, Name = "The Ananse Story" }
+                );
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
