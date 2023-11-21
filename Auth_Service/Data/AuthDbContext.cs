9134f00fdf89952b335d7aca20672d87a11a9857
= <=>
2023-11-21 13:56:07 +0000

    Added auth api service

---
+++ Auth_Service/Data/AuthDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth_Service.Data
+{
+    public class AuthDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) :base(options)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
